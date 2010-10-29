55130b08b8ff187cb3a3c5a14d5d0f5a9c85a59f
unknown <e198614@.global.ds.honeywell.com>
2010-10-28 21:29:46 -0500

    Fix CRLF

--- compiler/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make $* OSTYPE="${OSTYPE}" -f makefile
--- plugins/init.lua
+++
@@ -1,5 +0,0 @@
------------------------------------------------------
--- Global Lua Initialization Script for NESICIDE2  --
--- DO NOT MODIFY OR REPLACE THIS FILE!             --
------------------------------------------------------
-compiler_logger_print("Hello from external lua script!");
