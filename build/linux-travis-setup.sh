eeab5347dacbe3e51407635e1bdcce1c15bc107b
Christopher Pow <christopher.s.pow@gmail.com>
2017-10-23 22:09:30 -0500

    Fix for Linux Travis?

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -10,4 +10,5 @@ elif [ "$1" == "install" ]; then
   sudo apt-get -y install qt59base
   sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
   source /opt/qt*/bin/qt*-env.sh
+  exit 0
 fi
