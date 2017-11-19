6b807e859ad0ba82c8c950e3df88593889265248
Christopher Pow <christopher.s.pow@gmail.com>
2017-11-19 15:29:06 -0600

    Fix linux travis?

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -2,11 +2,11 @@
 set -v
 
 if [ "$1" == "before_install" ]; then
-#  sudo add-apt-repository ppa:beineri/opt-qt562-trusty -y
+  sudo add-apt-repository ppa:beineri/opt-qt562-trusty -y
   sudo add-apt-repository ppa:beineri/opt-qt591-trusty -y
   sudo apt-get update -qq
 elif [ "$1" == "install" ]; then
-#  sudo apt-get -y install qt56base
+  sudo apt-get -y install qt56base
   sudo apt-get -y install qt59base
   sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
   source /opt/qt*/bin/qt*-env.sh
