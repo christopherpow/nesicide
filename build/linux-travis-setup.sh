959deb972813bcde033c89d19c8c4061ba170b71
Christopher Pow <christopher.s.pow@gmail.com>
2018-12-02 13:14:50 -0600

    Travis crap.

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -2,14 +2,10 @@
 set -v
 
 if [ "$1" == "before_install" ]; then
-  sudo add-apt-repository ppa:beineri/opt-qt562-xenial -y
-  sudo add-apt-repository ppa:beineri/opt-qt591-xenial -y
-  sudo add-apt-repository ppa:beineri/opt-qt-5.10.1-xenial -y
+  sudo add-apt-repository ppa:beineri/opt-qt-5.11.2-xenial -y
   sudo apt-get update -qq
 elif [ "$1" == "install" ]; then
-  sudo apt-get -y install qt56base
-  sudo apt-get -y install qt59base
-  sudo apt-get -y install qt510base
+  sudo apt-get -y install qt511base
   sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
   source /opt/qt*/bin/qt*-env.sh
   exit 0
