c1a16bfbb3a97eb3ed2d61cfdcd54163706ff53c
Christopher Pow <christopher.s.pow@gmail.com>
2018-12-09 14:37:13 -0600

    Travis.

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -10,8 +10,8 @@ if [ "$1" == "before_install" ]; then
 elif [ "$1" == "install" ]; then
   sudo apt-get -y install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
   sudo apt-get -y install qt5-default qttools5-dev-tools
+  sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
   #sudo apt-get -y install qt511base
-  #sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
   source /opt/qt*/bin/qt*-env.sh
   exit 0
 fi
