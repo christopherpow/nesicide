636b787f6f4b4f8cc093398734f151efed8d4096
Christopher Pow <christopher.s.pow@gmail.com>
2018-12-12 17:13:32 -0600

    Travis.

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -2,16 +2,16 @@
 set -v
 
 if [ "$1" == "before_install" ]; then
-  sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-  sudo apt-get update -qq
+#  sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+#  sudo apt-get update -qq
 
-  #sudo add-apt-repository ppa:beineri/opt-qt-5.11.2-xenial -y
-  #sudo apt-get update -qq
+  sudo add-apt-repository ppa:beineri/opt-qt-5.10.1-xenial -y
+  sudo apt-get update -qy 
 elif [ "$1" == "install" ]; then
-  sudo apt-get -y install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
-  sudo apt-get -y install qt5-default qttools5-dev-tools
+#  sudo apt-get -y install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
+#  sudo apt-get -y install qt5-default qttools5-dev-tools
   sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
-  #sudo apt-get -y install qt511base
+  sudo apt-get -y install qt510base
   source /opt/qt*/bin/qt*-env.sh
   exit 0
 fi
