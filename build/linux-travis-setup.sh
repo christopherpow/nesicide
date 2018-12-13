515982d2e43e7009286dc3d423e9918f3692b977
Christopher Pow <christopher.s.pow@gmail.com>
2018-12-12 20:23:14 -0600

    Travis no qt4.

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -7,6 +7,7 @@ if [ "$1" == "before_install" ]; then
 
   sudo add-apt-repository -y ppa:beineri/opt-qt-5.10.1-xenial
   sudo apt-get -qy update 
+  sudo apt purge qt4*
 
 elif [ "$1" == "install" ]; then
   sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
