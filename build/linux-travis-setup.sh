0b465660a4754650d96847b2374883501e307102
Christopher Pow <christopher.s.pow@gmail.com>
2018-12-12 19:59:49 -0600

    Travis.

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -2,18 +2,19 @@
 set -v
 
 if [ "$1" == "before_install" ]; then
-  sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-  sudo apt-get update -qq
+#  sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+#  sudo apt-get update -qq
 
-#  sudo add-apt-repository ppa:beineri/opt-qt-5.10.1-xenial -y
-#  sudo apt-get update -qy 
+  sudo add-apt-repository -y ppa:beineri/opt-qt-5.10.1-xenial
+  sudo apt-get -qy update 
 
 elif [ "$1" == "install" ]; then
-  sudo apt-get -y install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
-  sudo apt-get -y install qt5-default qttools5-dev-tools
   sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
-#  sudo apt-get -y install qt510base
-  ls -al /opt/
+#  sudo apt-get -y install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
+#  sudo apt-get -y install qt5-default qttools5-dev-tools
+  sudo apt-get -qy install qt510base qt510xmlpatterns
+  export QMAKESPEC=linux-g++
+  . /opt/qt510/bin/qt510-env.sh
   export QT_SELECT=qt5
   exit 0
 fi
