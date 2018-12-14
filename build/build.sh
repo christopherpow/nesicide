001f0fa9dfebe6cb73f1b1309ac3cada3b373333
Christopher Pow <christopher.s.pow@gmail.com>
2018-12-13 21:06:27 -0600

    Trav.

--- build/build.sh
+++ build/build.sh
@@ -14,10 +14,10 @@ fi
 
 # add CONFIG+=debug to qmake to build debug.
 echo Building NESICIDE...
-qmake build/ide/ide.pro; make -C build/ide
+qmake build/ide/ide.pro; make -C build/ide -f Makefile
 echo Building FamiTracker...
-qmake build/famitracker/famitracker.pro; make -C build/famitracker
+qmake build/famitracker/famitracker.pro; make -C build/famitracker -f Makefile
 echo Building FamiPlayer...
-qmake build/famiplayer/famiplayer.pro; make -C build/famiplayer
+qmake build/famiplayer/famiplayer.pro; make -C build/famiplayer -f Makefile
 echo Building NES Emulator...
-qmake build/nes-emulator/nes-emulator.pro; make -C build/nes-emulator
+qmake build/nes-emulator/nes-emulator.pro; make -C build/nes-emulator -f Makefile
