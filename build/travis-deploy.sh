07957611f5ab8c0e1aebc08fd0aff038c986e54e
Christopher Pow <christopher.s.pow@gmail.com>
2019-01-03 21:27:52 -0600

    OFFS Travis.

--- build/travis-deploy.sh
+++ build/travis-deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ls -al $TRAVIS_BUILD_DIR
+ls -al $TRAVIS_BUILD_DIR/build
+
 rsync $TRAVIS_BUILD_DIR/dist/*.dmg cpow@162.243.126.83:/var/www/html/nesicide/
 rsync $TRAVIS_BUILD_DIR/dist/*.AppImage cpow@162.243.126.83:/var/www/html/nesicide/
 
