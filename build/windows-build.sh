89c9dc02f438f0373fa4b0d355d7645b3fcc0e08
U-MS\PowC <PowC@EPPOWCH.ms.starkey.com>
2019-01-08 09:19:00 -0600

    Travis Windows.

--- build/windows-build.sh
+++ build/windows-build.sh
@@ -9,6 +9,7 @@ if [ ! -d "deps" ]; then
    echo "Fetching dependencies package..."
    wget https://knob.phreneticappsllc.com/nesicide/nesicide-deps.tar.bz2
    echo "Extracting dependencies package..."
+   tar --help
    tar xjf --checkpoint=1000 --checkpoint-action=dot nesicide-deps.tar.bz2
    rm -f nesicide-deps.tar.bz2
 else
