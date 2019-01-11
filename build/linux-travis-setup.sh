b4a3d4b7bef1d278b52ce0fba463684b6c61c0ba
U-MS\PowC <PowC@EPPOWCH.ms.starkey.com>
2019-01-11 15:22:01 -0600

    Travis trying to get it working.

--- build/linux-travis-setup.sh
+++ build/linux-travis-setup.sh
@@ -2,16 +2,12 @@
 set -v
 
 if [ "$1" == "before_install" ]; then
-#  sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-#  sudo apt-get update -qq
-
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 762E3157
   sudo add-apt-repository -y ppa:beineri/opt-qt-5.10.1-xenial
   sudo apt-get -qy update 
   sudo apt purge qt4*
 
 elif [ "$1" == "install" ]; then
   sudo apt-get -y install build-essential libasound2-dev liblua5.1-dev libsdl1.2-dev libgl1-mesa-dev wine-dev
-#  sudo apt-get -y install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
-#  sudo apt-get -y install qt5-default qttools5-dev-tools
   sudo apt-get -qy install qt510base qt510xmlpatterns
 fi
