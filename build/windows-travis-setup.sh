abf3fc29b2ee6b6b3554ece75a8e7ff50e36de95
U-MS\PowC <PowC@EPPOWCH.ms.starkey.com>
2019-01-14 09:50:00 -0600

    Windows Qt.

--- build/windows-travis-setup.sh
+++ build/windows-travis-setup.sh
@@ -2,7 +2,7 @@
 set -v
 
 if [ "$1" == "install" ]; then
-  curl -Lo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe
+  curl -vLo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe
   if ! ~/qt-unified-windows-x86-online.exe --verbose --script build/windows-qt-installer.qs > ~/qt-installer-output.txt; then
     cat ~/qt-installer-output.txt; exit 1
   fi
