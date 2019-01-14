9e673cbb3e72963f581b4792b387f15de69a7b21
U-MS\PowC <PowC@EPPOWCH.ms.starkey.com>
2019-01-14 09:10:10 -0600

    Windows Travis.

--- build/windows-travis-setup.sh
+++ build/windows-travis-setup.sh
@@ -2,5 +2,8 @@
 set -v
 
 if [ "$1" == "install" ]; then
-  choco install qtcreator -ry
+  curl -vLo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe
+  if ! ~/qt-unified-windows-x86-online.exe --verbose --script tools/qt-installer-windows.qs > ~/qt-installer-output.txt; then
+    cat ~/qt-installer-output.txt; exit 1
+  fi
 fi
