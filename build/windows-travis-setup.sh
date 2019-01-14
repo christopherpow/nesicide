80dd25912ceffa76ac3a655f027d5aca551f941d
U-MS\PowC <PowC@EPPOWCH.ms.starkey.com>
2019-01-14 08:12:31 -0600

    Windows Travis.

--- build/windows-travis-setup.sh
+++ build/windows-travis-setup.sh
@@ -2,7 +2,5 @@
 set -v
 
 if [ "$1" == "install" ]; then
-  choco install qtcreator -ry
-  ls -al c:/tools/qtcreator/bin
-  export PATH="c:/tools/qtcreator/bin/":$PATH
+  choco install qtcreator -rvy
 fi
