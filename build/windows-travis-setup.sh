189d75c1b301a458c910283e4387f736366495a0
Christopher Pow <christopher.s.pow@gmail.com>
2019-01-14 20:37:31 -0600

    Windows travis.

--- build/windows-travis-setup.sh
+++ build/windows-travis-setup.sh
@@ -3,10 +3,10 @@ set -v
 
 if [ "$1" == "before_install" ]; then
   echo "Downloading Qt..."
-  curl -vLo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe 2>&1
+  curl -Lo ~/qt-unified-windows-x86-online.exe http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe 2>&1
   echo "Done downloading."
 elif [ "$1" == "install" ]; then
   echo "Running Qt installer..."
-  ~/qt-unified-windows-x86-online.exe --verbose --script build/windows-qt-installer.qs 2>&1
+  ~/qt-unified-windows-x86-online.exe --script build/windows-qt-installer.qs 2>&1
   echo "Done installing Qt."
 fi
