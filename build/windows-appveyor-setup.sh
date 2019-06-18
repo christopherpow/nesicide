72ac09db3a3c92597c6f5b356b681ecaf23e83ce
Christopher Pow <Christopher_Pow@starkey.com>
2019-06-18 13:39:28 -0500

    Appveyor setup qt.

--- build/windows-appveyor-setup.sh
+++ build/windows-appveyor-setup.sh
@@ -5,5 +5,5 @@ if [ "$1" == "install" ]; then
   echo "Getting make..."
   choco install make -y
   echo "Getting Qt..."
-  choco install qtcreator -y
+  choco install qtcreator --version 4.9.0 -y
 fi
