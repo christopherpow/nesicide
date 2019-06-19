2a5676285c35b83dc84f093fbac41ae35f5f7dbf
Christopher Pow <Christopher_Pow@starkey.com>
2019-06-19 11:51:41 -0500

    Appveyor.

--- build/windows-build.sh
+++ build/windows-build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-PATH=deps\Windows\GnuWin32\bin:$PATH
+#PATH=deps/Windows/GnuWin32/bin:$PATH
 QTDIR=/c/Qt/5.11.3/mingw53_32
 PATH=$PATH:$QTDIR/bin
+
+ls -al /c/Qt/
+ls -al /c/Qt/5.11.3/
 
 # check for deps
 echo -n "Checking for dependencies package..."
