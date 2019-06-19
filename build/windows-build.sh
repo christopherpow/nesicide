6060183ecfefc5cfef2d43d5afe7a57dcbc55503
Christopher Pow <Christopher_Pow@starkey.com>
2019-06-19 12:25:22 -0500

    Appveyor.

--- build/windows-build.sh
+++ build/windows-build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#PATH=deps/Windows/GnuWin32/bin:$PATH
-QTDIR=/c/Qt/5.11.3/mingw53_32
-PATH=$PATH:$QTDIR/bin
-
 # check for deps
 echo -n "Checking for dependencies package..."
 if [ ! -d "deps" ]; then
@@ -17,10 +13,10 @@ else
 fi
 # add CONFIG+=debug to qmake to build debug.
 echo Building NESICIDE...
-( cd build/ide; qmake; mingw64-make )
+( cd build/ide; qmake; mingw32-make )
 echo Building FamiTracker...
-( cd build/famitracker; qmake; mingw64-make )
+( cd build/famitracker; qmake; mingw32-make )
 echo Building FamiPlayer...
-( cd build/famiplayer; qmake; mingw64-make )
+( cd build/famiplayer; qmake; mingw32-make )
 echo Building NES Emulator...
-( cd build/nes-emulator; qmake; mingw64-make )
+( cd build/nes-emulator; qmake; mingw32-make )
