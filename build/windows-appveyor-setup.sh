00410e7aee55ac25d1ff13bc6c63e7de1c424d8a
Christopher Pow <christopher.s.pow@gmail.com>
2019-06-19 21:09:13 -0500

    Appveyor...

--- build/windows-appveyor-setup.sh
+++ build/windows-appveyor-setup.sh
@@ -4,5 +4,5 @@ set -v
 if [ "$1" == "before_install" ]; then
   echo "Nothing to do for before_install..."
 elif [ "$1" == "install" ]; then
-  echo "Nothing to do for install..."
+  choco install rsync
 fi
