34dec59103a28325bd66e9ceb4086ab2b6387df0
Christopher Pow <Christopher_Pow@starkey.com>
2019-10-08 10:52:19 -0500

    Appveyor.

--- build/windows-appveyor-setup.sh
+++ build/windows-appveyor-setup.sh
@@ -5,6 +5,5 @@ if [ "$1" == "before_install" ]; then
   echo "Nothing to do for before_install..."
 elif [ "$1" == "install" ]; then
   choco install rsync -y
-  refreshenv
-  rsync --help
+  which rsync
 fi
