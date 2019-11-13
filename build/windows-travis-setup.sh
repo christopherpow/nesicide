b255e353ff645e755d51a55570912184dac971f1
Christopher Pow <Christopher_Pow@starkey.com>
2019-11-13 15:26:16 -0600

    Trying to set qmake in path.

--- build/windows-travis-setup.sh
+++ build/windows-travis-setup.sh
@@ -5,4 +5,6 @@ if [ "$1" == "before_install" ]; then
   echo "Nothing to do for before_install..."
 elif [ "$1" == "install" ]; then
   choco install qtcreator -y
+  export PATH=/c/tools/qtcreator:$PATH
+  which qmake
 fi
