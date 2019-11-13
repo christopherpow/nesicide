140c8a499fe733c6bd24f616105debc7d6a364d8
Christopher Pow <Christopher_Pow@starkey.com>
2019-11-13 15:16:14 -0600

    Adding windows Travis setup.

--- build/windows-travis-setup.sh
+++ build/windows-travis-setup.sh
@@ -4,6 +4,5 @@ set -v
 if [ "$1" == "before_install" ]; then
   echo "Nothing to do for before_install..."
 elif [ "$1" == "install" ]; then
-  choco install rsync -y
-  choco install qt -y
+  choco install qtcreator -y
 fi
