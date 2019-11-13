a6265fc92928e88838758c2f530ee2cbdb2e5db7
Christopher Pow <Christopher_Pow@starkey.com>
2019-11-13 15:06:30 -0600

    Adding windows Travis setup.

---
+++ build/windows-travis-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -v
+
+if [ "$1" == "before_install" ]; then
+  echo "Nothing to do for before_install..."
+elif [ "$1" == "install" ]; then
+  choco install rsync -y
+  which rsync
+fi
