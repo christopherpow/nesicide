14c32d601e02f8d6227020166b3c52d7226c9eb3
Christopher Pow <christopher.s.pow@gmail.com>
2020-03-21 13:53:38 -0500

    Clean script updates.

--- build/clean.sh
+++ build/clean.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function really_clean() {
+  for f in apps/{ide,fami*,nes*} libs/{nes,c64,fami*} deps/{rtmidi,qscintilla2/Qt4Qt5}; do echo $f; ( rm -rfv $f/debug $f/release $f/Makefile* $f/.qmake* ); done
+}
+
 echo Cleaning NESICIDE...
 ( cd build/ide; make distclean )
 echo Cleaning FamiTracker...
@@ -8,6 +12,9 @@ echo Cleaning FamiPlayer...
 ( cd build/famiplayer; make distclean )
 echo Cleaning NES Emulator...
 ( cd build/nes-emulator; make distclean )
+echo Making sure all build debris is GONE...
+really_clean
+
 if [ "$1" == "deps" ]; then
   echo Removing deps...
   rm -rf deps
